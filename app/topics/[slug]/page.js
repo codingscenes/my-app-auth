1cdba94d0ca33259d42bb555e1b9c0d7b84d2504
Coding Scenes <codingscenes@gmail.com>
2023-12-17 01:42:49 +0530

    showing slug on topic page and adding containers

--- app/topics/[slug]/page.js
+++ app/topics/[slug]/page.js
@@ -1,5 +1,14 @@
-export default function TopicPage() {
+export default function TopicPage({ params }) {
+
+  const { slug } = params;
+
   return (
-    <div>TopicPage</div>
+    <div className='grid grid-cols-4 gap-4 p-4'>
+      <div className="col-span-3">
+        <h1 className="text-2xl font-bold-">
+          {slug}
+        </h1>
+      </div>
+    </div>
   )
 }
