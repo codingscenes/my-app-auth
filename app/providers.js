fc4df10979bca196481dedbdc6b8ba0c57043c4a
Coding Scenes <codingscenes@gmail.com>
2023-12-16 17:25:31 +0530

    accessing session on client

--- app/providers.js
+++ app/providers.js
@@ -1,12 +1,14 @@
 'use client';
 
 import { NextUIProvider } from '@nextui-org/react';
-import { SessionProvider } from 'next-auth/react';
+import { SessionProvider } from 'next-auth/react'; // for accessing session on client component using useSession() hook fom 'next-auth/react'; ->
+// session = useSession()
+// session.data?.user
 
 // sharing next ui state to all components eg modal etc.
 export default function Providers({ children }) {
-  return
+  return;
   <SessionProvider>
-  <NextUIProvider>{children}</NextUIProvider>
-  </SessionProvider>
+    <NextUIProvider>{children}</NextUIProvider>
+  </SessionProvider>;
 }
